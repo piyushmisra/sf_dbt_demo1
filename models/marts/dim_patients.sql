5e7ed8603678bcb10b5740de80344399bd8ff729
piyushmisra <piyush_misra@hotmail.com>
2026-02-05 15:03:52 +1300

    pre making mods for pro grade scd2

--- models/marts/dim_patients.sql
+++ models/marts/dim_patients.sql
@@ -7,54 +7,68 @@
 
 with source as (
     select
-        id,
+        id as natural_key,
         first_name,
         last_name,
-        {{ dbt_utils.generate_surrogate_key(['id']) }} as natural_key,
-        {{ dbt_utils.generate_surrogate_key(['id','first_name','last_name']) }} as scd_hash,
+        abs(hash(id, first_name, last_name)) as scd_hash,
         current_timestamp() as updated_at
     from {{ ref('stg_seed__patients') }}
-),
+)
 
 {% if is_incremental() %}
-current_target as (
+, current_target as (
     select *
     from {{ this }}
     where is_current = true
-),
+)
 
-records_to_insert as (
+, rows_to_expire as (
     select
-        s.id,
+        t.natural_key,
+        t.first_name,
+        t.last_name,
+        t.scd_hash,
+        t.valid_from,
+        current_timestamp() as valid_to,
+        false as is_current
+    from current_target t
+    join source s
+        on s.natural_key = t.natural_key
+    where s.scd_hash != t.scd_hash
+)
+{% endif %}
+
+, new_rows as (
+    select
+        s.natural_key,
         s.first_name,
         s.last_name,
-        s.natural_key,
         s.scd_hash,
         current_timestamp() as valid_from,
         null as valid_to,
         true as is_current
     from source s
+    {% if is_incremental() %}
     left join current_target t
         on s.natural_key = t.natural_key
     where t.natural_key is null
        or s.scd_hash != t.scd_hash
+    {% endif %}
 )
-{% else %}
-records_to_insert as (
-    select
-        id,
-        first_name,
-        last_name,
-        natural_key,
-        scd_hash,
-        current_timestamp() as valid_from,
-        null as valid_to,
-        true as is_current
-    from source
-)
-{% endif %}
 
 select
-    {{ dbt_utils.generate_surrogate_key(['natural_key','valid_from']) }} as patient_sk,
-    *
-from records_to_insert
+    abs(hash(natural_key, valid_from)) as patient_sk,
+    natural_key,
+    first_name,
+    last_name,
+    scd_hash,
+    valid_from,
+    valid_to,
+    is_current
+from (
+    {% if is_incremental() %}
+    select * from rows_to_expire
+    union all
+    {% endif %}
+    select * from new_rows
+)
